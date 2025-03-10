13e822338e5e863e1babd333ec1f12443542fed5
CesarDty <cesaraugustocabreramarcos51@gmail.com>
2025-03-09 18:45:14 -0600

    Contiene Pantallas De Eegistro y Login

--- app/src/main/java/com/uttt/gogirls/MainActivity.kt
+++ app/src/main/java/com/uttt/gogirls/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
             GoGirlsTheme {
                 Surface() {
                     navController= rememberNavController()
+
                     RootNavGraph(navHostController=navController)
                 }
             }
